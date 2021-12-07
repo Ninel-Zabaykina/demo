6e84cd7cc90f7d4cca576a62041315283d504d39
Ninel-Zabaykina <zabaykinanm@mer.ci.nsu.ru>
2021-12-07 12:57:43 +0700

    adding models of request

--- portal/models.py
+++ portal/models.py
@@ -2,8 +2,24 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200, help_text="Укажите категорию заявки")
+
+    def __str__(self):
+        return self.name
+
+
 class Portal(models.Model):
     STATUS = (
         ('решена', 'Решена'),
         ('отклонена', 'Отклонена'),
+        ('новая', 'Новая')
     )
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS, default='Новая')
+    categories = models.ManyToManyField(Categories, help_text="Укажите категорию заявки")
+    photo = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=100, null=True)
+
