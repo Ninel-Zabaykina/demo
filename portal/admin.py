48494944cf981b720490482e805b12d901683877
Ninel-Zabaykina <zabaykinanm@mer.ci.nsu.ru>
2021-12-11 23:21:50 +0700

    adding admin panel

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Portal
+from .models import Categories
 
-# Register your models here.
+admin.site.register(Portal)
+admin.site.register(Categories)
+
