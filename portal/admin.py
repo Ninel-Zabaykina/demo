56fd0a0e6c370293de4101b0a74ec6ed58e432dd
Ninel-Zabaykina <zabaykinanm@mer.ci.nsu.ru>
2021-12-12 00:35:25 +0700

    adding list app and detail view app

--- portal/admin.py
+++ portal/admin.py
@@ -3,6 +3,15 @@ from .models import Portal
 from .models import Categories
 
 
+#class AppAdmin(admin.ModelAdmin):
+ #   list_display = ('title', 'author', 'publish', 'status')
+  #  list_filter = ('status', 'publish', 'author')
+   # search_fields = ('title', 'body')
+    #raw_id_fields = ('author',)
+    #date_hierarchy = 'publish'
+    #ordering = ['status', 'publish']
+
+
 admin.site.register(Portal)
 admin.site.register(Categories)
 
--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Portal
 
-# Create your views here.
+
+def app_list(request):
+    app = Portal.publish.all()
+    return render(request, 'portal/app/list.html', {'app': app})
+
+
+def app_detail(request, year, month, day, app):
+    app = get_object_or_404(Portal, title=app,
+                            status='publish',
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
+    return render(request, 'portal/app/detail.html', {'app': app})
